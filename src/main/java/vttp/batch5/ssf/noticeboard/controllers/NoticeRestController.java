5967d3ebd0b128e3256e3982815403502a685297
joycetan913 <joycetanyy913@gmail.com>
2024-12-13 16:46:18 +0800

    initial commit

--- src/main/java/vttp/batch5/ssf/noticeboard/controllers/NoticeRestController.java
+++ src/main/java/vttp/batch5/ssf/noticeboard/controllers/NoticeRestController.java
@@ -20,43 +20,55 @@ import org.springframework.web.client.RestTemplate;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import vttp.batch5.ssf.noticeboard.models.Notice;
+import vttp.batch5.ssf.noticeboard.services.NoticeService;
 @RestController
 @RequestMapping
 public class NoticeRestController {
     
     @Autowired
     NoticeService noticeService;
+    public static final String POST_URL = "http://localhost:8080/notice";
     // POST /notice
    // Content-Type: application/json
    // Accept: application/json
    @PostMapping(path="/notice", produces = MediaType.APPLICATION_JSON_VALUE
          , consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> postAsJsonBody(@RequestBody String payload) {
+    public ResponseEntity<String> postAsJsonBody() {
         //cr8 payload
         Notice n = new Notice();
         JsonObject json = Json.createObjectBuilder()
           .add("title", n.getTitle())
-          .add("poster", n.())
+          .add("poster", n.getPoster())
+          .add("postDate", String.valueOf(n.getPostDate()))
+          .add("categories", n.getCategories())
+          .add("text", n.getText())
           .build();
 
         // Create a request
         RequestEntity<String> req = RequestEntity
+            .post(POST_URL)
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
             .body(json.toString(), String.class);
 
         RestTemplate template = new RestTemplate();
 
-        ResponseEntity<String> resp = template.exchange(req, String.class);
-        String payload = json.getBody();
-       
-        if (resp != null) {
-            //Return the article with a 200 OK status
-            return ResponseEntity.ok(resp.toString());
-        } else {
-            //If article not found, return 404 Not Found
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try {
+            ResponseEntity<String> resp = template.exchange(req, String.class);
+            String payload = resp.getBody();
+            System.out.printf(">> %s\n", payload);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
-        System.out.printf(">>> PAYLOAD: %s\n", payload);
+        // if (resp != null) {
+        //     //Return the article with a 200 OK status
+        //     return ResponseEntity.ok(resp.toString());
+        // } else {
+        // //If article not found, return 404 Not Found
+        //  return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // }
+      
     }
      //GET /status
     // Content-Type: application/json
